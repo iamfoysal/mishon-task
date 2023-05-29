1524a1ca8e9399745db545ef88e9cd4ee8cb0394
git-foysal-1 <foysal.devxhub@gmail.com>
2023-05-29 23:42:28 +0600

    admin config and static root setup

--- core/settings.py
+++ core/settings.py
@@ -108,10 +108,8 @@ REST_FRAMEWORK = {
 }
 
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 
 
 LANGUAGE_CODE = 'en-us'
@@ -126,10 +124,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
