13ac83756dcb05b47044e5e54d675c6faa6d39e4
git-foysal-1 <foysal.devxhub@gmail.com>
2023-05-31 00:13:17 +0600

    product filter with multi cat

--- shop/models.py
+++ shop/models.py
@@ -83,7 +83,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     image = models.ImageField(upload_to='product/')
     warranty = models.ForeignKey(WarrantyPeriod, on_delete=models.CASCADE)
-    is_available = models.BooleanField(default=True,null=True)
+    is_available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
     def __str__(self):
